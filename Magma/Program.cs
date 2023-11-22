3a20974616b9b2494576f45925e6225cef7ef47e
Faso367 <63189651+Faso367@users.noreply.github.com>
2023-11-22 23:39:35 +0500

    Update Program.cs

--- Magma/Program.cs
+++ Magma/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Linq;
 using System.Text;
 using ExtensionMethods;
@@ -9,24 +9,39 @@ namespace Magma
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите открытый текст в 16-ричной СС: ");
-            string openTextForCFB  = Console.ReadLine();
-            string key = "ffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
-            string IV = "1234567890abcdef234567890abcdef1";
-            //string openTextForCFB = "92def06b3c130a59db54c704f8189d204a98fb2e67a8024c8912409b17b57e41";
+            try
+            {
+                Console.WriteLine("Введите открытый текст в 16-ричной СС: ");
+                string openTextForCFB = Console.ReadLine();
 
-            byte[] byte_CFB_message = Convert.FromHexString(openTextForCFB);
-            byte[] byte_key = key.ToByteArray();
-            byte[] byte_IV = Convert.FromHexString(IV);
+                string key = "ffeeddccbbaa99887766554433221100f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+                string IV = "1234567890abcdef234567890abcdef1";
+                //string openTextForCFB = "92def06b3c130a59db54c704f8189d204a98fb2e67a8024c8912409b17b57e41";
 
-            Console.WriteLine($"Ключ шифрования:\n{byte_key.ToHexString()}");
-            Console.WriteLine($"Синхропосылка:\n{byte_IV.ToHexString()}\n");
+                byte[] byte_CFB_message = Convert.FromHexString(openTextForCFB);
+                byte[] byte_key = key.ToByteArray();
+                byte[] byte_IV = Convert.FromHexString(IV);
 
-            byte[] encryptMessage = Magma.CFBEncrypt(byte_IV, byte_CFB_message, byte_key);
-            Console.WriteLine($"Зашифрованное сообщение:\n{encryptMessage.ToHexString()}");
+                Console.WriteLine($"Ключ шифрования:\n{byte_key.ToHexString()}");
+                Console.WriteLine($"Синхропосылка:\n{byte_IV.ToHexString()}\n");
 
-            Console.WriteLine($"Расшифрованное сообщение:\n{Magma.CFBDecrypt(byte_IV, encryptMessage, byte_key).ToHexString()}");
+                byte[] encryptMessage = Magma.CFBEncrypt(byte_IV, byte_CFB_message, byte_key);
+                Console.WriteLine($"Зашифрованное сообщение:\n{encryptMessage.ToHexString()}");
 
+                Console.WriteLine($"Расшифрованное сообщение:\n{Magma.CFBDecrypt(byte_IV, encryptMessage, byte_key).ToHexString()}");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine($"Сообщение об ошибке: {ex}\n"
+                    + "\n   --------------   Внимание!!!   --------------\n" +
+                    " Длина сообщения должна быть кратна двум. \n" +
+                    " Сообщение должно быть введено в шестнадцатеричном формате.\n" +
+                    " Шестнадцатеричный алфавит включает символы 0123456789abcdef");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Возникла непредвиденная ошибка: {ex}");
+            }
             // Должны получить  db37e0e266903c83 0d46644c1f9a089c 24bdd2035315d38b bcc0321421075505
         }
     }
